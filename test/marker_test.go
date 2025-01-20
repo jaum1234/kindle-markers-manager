758220ee6f22f779e4ef489f1427ceacdbc14d2c
joao coura <joaovitorsouzacoura@gmai.com>
2025-01-19 21:21:27 -0300

    :sparkles: feat: writing table-drive unit tests for the marker tests.

--- test/marker_test.go
+++ test/marker_test.go
@@ -3,32 +3,45 @@ package test
 import (
 	"coura/kindlemanager/internal/marker"
 	"testing"
-	"time"
 )
 
 func TestExtractPositions(t *testing.T) {
-	l := "Seu destaque na página 66 | posição 1011-1013 | Adicionado: segunda-feira, 21 de outubro de 2024 17:32:25"
-
-	start, end := marker.ExtractPositions(l)
-
-	if start != "1011" {
-		t.Errorf("Expected 1011, got %s", start)
+	tests := []struct {
+		name       string
+		l          string
+		start, end string
+	}{
+		{"Should extract the positions 1011 and 1013", "Seu destaque na página 66 | posição 1011-1013 | Adicionado: segunda-feira, 21 de outubro de 2024 17:32:25", "1011", "1013"},
+		{"Should extract the positions 1 and 3", "Seu destaque na página 66 | posição 1-3 | Adicionado: segunda-feira, 21 de outubro de 2024 17:32:25", "1", "3"},
 	}
 
-	if end != "1013" {
-		t.Errorf("Expected 1013, got %s", end)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			start, end := marker.ExtractPositions(tt.l)
+
+			if start != tt.start || end != tt.end {
+				t.Errorf("Expected %s-%s, got %s-%s", tt.start, tt.end, start, end)
+			}
+		})
 	}
 }
 
 func TestCalculateTimestamp(t *testing.T) {
-	l := "Seu destaque na página 66 | posição 1011-1013 | Adicionado: segunda-feira, 21 de outubro de 2024 17:32:25"
-	time, _ := time.Parse("2006-01-02 15:04:05", "2024-10-21 17:32:25")
+	tests := []struct {
+		name     string
+		l        string
+		expected int64
+	}{
+		{"Should calculate the timestamp 1729531945", "Seu destaque na página 66 | posição 1011-1013 | Adicionado: segunda-feira, 21 de outubro de 2024 17:32:25", 1729531945},
+	}
 
-	expected := int64(time.Unix())
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := marker.CalculateTimestamp(tt.l)
 
-	result := marker.CalculateTimestamp(l)
-
-	if result != expected {
-		t.Errorf("Expected %d, got %d", expected, result)
+			if result != tt.expected {
+				t.Errorf("Expected %d, got %d", tt.expected, result)
+			}
+		})
 	}
 }
