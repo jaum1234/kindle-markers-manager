f62e085b7a6e08d1b2d82511c834a4d2bc47824c
joao coura <joaovitorsouzacoura@gmai.com>
2025-03-17 23:55:54 -0300

    tests: tests extractBookTitle function

--- src/marker/marker.test.ts
+++ src/marker/marker.test.ts
@@ -1,4 +1,4 @@
-import { extractAuthor } from "./marker";
+import { extractAuthor, extractBookTitle } from "./marker";
 
 describe("Unit tests for Marker", () => {
     it("Should extract the author name when between parentheses", () => {
@@ -13,6 +13,30 @@ describe("Unit tests for Marker", () => {
         const input = "Computer Networking: A Top-Down Approach, 7/e - James Kurose;Keith Ross";
         const expected = "James Kurose;Keith Ross";
         const result = extractAuthor(input);
+
+        expect(result).toBe(expected);
+    });
+
+    it("Should extract the book title when succeeded by parentheses", () => {
+        const input = "Computer Networking: A Top-Down Approach, 7/e (James Kurose;Keith Ross)";
+        const expected = "Computer Networking: A Top-Down Approach, 7/e";
+        const result = extractBookTitle(input);
+
+        expect(result).toBe(expected);
+    });
+
+    it("Should extract the book title when succeeded by a dash", () => {
+        const input = "Computer Networking: A Top-Down Approach, 7/e - James Kurose;Keith Ross";
+        const expected = "Computer Networking: A Top-Down Approach, 7/e";
+        const result = extractBookTitle(input);
+
+        expect(result).toBe(expected);
+    });
+
+    it("Should extract the book title when no author is present", () => {
+        const input = "Computer Networking: A Top-Down Approach, 7/e";
+        const expected = "Computer Networking: A Top-Down Approach, 7/e";
+        const result = extractBookTitle(input);
 
         expect(result).toBe(expected);
     });
