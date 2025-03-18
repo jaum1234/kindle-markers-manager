a5879b6f87d5e090bbed10ec11e4ad26cc2bb739
joao coura <joaovitorsouzacoura@gmai.com>
2025-03-18 08:28:01 -0300

    tests: test extractPage function

--- src/marker/marker.test.ts
+++ src/marker/marker.test.ts
@@ -1,4 +1,4 @@
-import { extractAuthor, extractBookTitle } from "./marker";
+import { extractAuthor, extractBookTitle, extractPage } from "./marker";
 
 describe("Unit tests for Marker", () => {
     it("Should extract the author name when between parentheses", () => {
@@ -37,6 +37,22 @@ describe("Unit tests for Marker", () => {
         const input = "Computer Networking: A Top-Down Approach, 7/e";
         const expected = "Computer Networking: A Top-Down Approach, 7/e";
         const result = extractBookTitle(input);
+
+        expect(result).toBe(expected);
+    });
+
+    it("Should extract the page when the input is in portuguese", () => {
+        const input = "Seu destaque na página 66 | posição 1011-1013 | Adicionado: segunda-feira, 21 de outubro de 2024 17:32:25";
+        const expected = "66";
+        const result = extractPage(input);
+
+        expect(result).toBe(expected);
+    });
+
+    it("Should extract the page when the input is in english", () => {
+        const input = "Your highlight on page 66 | location 1011-1013 | Added on Monday, October 21, 2024 5:32:25 PM";
+        const expected = "66";
+        const result = extractPage(input);
 
         expect(result).toBe(expected);
     });
