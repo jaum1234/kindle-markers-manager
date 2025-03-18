408a2c6cefe9f0fc04cf89f12b4345517ed4275d
joao coura <joaovitorsouzacoura@gmai.com>
2025-03-18 09:00:30 -0300

    feat: add function to convert month to number"

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,31 @@
 import { UNKNOWN } from "../constants";
 
+const monthToNumber = (month: string, language: string = "en"): string => {
+    let months: Array<string>;
+
+    if (language === "en") {
+        months = [
+            "january", "february", "march", "april", "may", "june",
+            "july", "august", "september", "october", "november", "december"
+        ];
+    } else {
+        months = [
+            "janeiro", "fevereiro", "março", "abril", "maio", "junho",
+            "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
+        ];
+    }
+
+    const numbers = [
+        "01", "02", "03", "04", "05", "06",
+        "07", "08", "09", "10", "11", "12"
+    ];
+
+    return months.reduce((obj, key, index) => {
+        obj[key] = numbers[index];
+        return obj;
+    }, {} as { [key: string]: string })[month.toLowerCase()];
+}
+
 const matchOrUnkown = (text: string, pattern: RegExp): string => {
     const match = text.match(pattern);
 
@@ -11,5 +37,6 @@ const matchOrUnkown = (text: string, pattern: RegExp): string => {
 }
 
 export {
-    matchOrUnkown
+    matchOrUnkown,
+    monthToNumber
 };
