486d39170872affe9e1834dac9289337180aab75
joao coura <joaovitorsouzacoura@gmai.com>
2025-03-18 08:40:38 -0300

    tests: tests extractPositions function

--- src/marker/marker.test.ts
+++ src/marker/marker.test.ts
@@ -1,4 +1,4 @@
-import { extractAuthor, extractBookTitle, extractPage } from "./marker";
+import { extractAuthor, extractBookTitle, extractPage, extractPositions } from "./marker";
 
 describe("Unit tests for Marker", () => {
     it("Should extract the author name when between parentheses", () => {
@@ -55,5 +55,21 @@ describe("Unit tests for Marker", () => {
         const result = extractPage(input);
 
         expect(result).toBe(expected);
+    });
+
+    it("Should extract the position when the input is in portuguese", () => {
+        const input = "Seu destaque na página 66 | posição 1011-1013 | Adicionado: segunda-feira, 21 de outubro de 2024 17:32:25";
+        const expected = ["1011", "1013"];
+        const result = extractPositions(input);
+
+        expect(result).toStrictEqual(expected);
+    });
+
+    it("Should extract the position when the input is in english", () => {
+        const input = "Your highlight on page 66 | location 2-4 | Added on Monday, October 21, 2024 5:32:25 PM";
+        const expected = ["2", "4"];
+        const result = extractPositions(input);
+
+        expect(result).toStrictEqual(expected);
     });
 });
