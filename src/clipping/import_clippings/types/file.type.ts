64048ac781fb958424987dda74828fe081e4972b
joao coura <joaovitorsouzacoura@gmai.com>
2025-03-27 12:15:05 -0300

    Move files to domain dir

--- src/clipping/import_clippings/types/file.type.ts
+++
@@ -1,5 +0,0 @@
-import { RawClipping } from "./raw_clipping.type"
-
-export type File {
-    parse: () => Promise<RawClipping[]>
-} 
--- src/clipping/import_clippings/types/raw_clipping.type.ts
+++
@@ -1,9 +0,0 @@
-export type RawClipping = {
-    extractAuthor: () => string;
-    extractType: () => string;
-    extractPage: () => string;
-    extractPositions: () => [string, string];
-    extractContent: () => string;
-    calculateTimestamp: () => number;
-    extractBookTitle: () => string;
-}
