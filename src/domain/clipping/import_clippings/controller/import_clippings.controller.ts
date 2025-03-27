c8b642d4483c3b1f83073666cf7c69d2ca013c6a
joao coura <joaovitorsouzacoura@gmai.com>
2025-03-27 12:21:28 -0300

    feat: render main page

--- src/domain/clipping/import_clippings/controller/import_clippings.controller.ts
+++ src/domain/clipping/import_clippings/controller/import_clippings.controller.ts
@@ -1,26 +1,32 @@
 import busboy from "busboy";
 import { Request, Response } from "express";
+import { ROOT } from "../../../../constants";
 import Clipping from "../../clipping.entity";
 import importClippingsUsecase from "../import_clippings.usecase";
 import File from "../services/file/file.service";
 
-const importClippings = (req: Request, res: Response) => {
-    const bb = busboy({ headers: req.headers });
-
-    let clippings: Clipping[];
-
-    bb.on("file", (name, file) => {
-        console.log(`[${name}] found, starting import.`);
-        importClippingsUsecase(new File(file)).then(c => clippings = c);
-    });
-
-    bb.on("close", () => {
-        res.status(201).json({ data: clippings.map((c) => c.toDTO()) });
-    });
-
-    req.pipe(bb);
+export const renderPage = () => {
+    return (req: Request, res: Response) => {
+        res.sendFile("public/index.html", { root: ROOT });
+    };
 }
 
-export {
-    importClippings
-};
+export const importClippings = () => {
+    return (req: Request, res: Response) => {
+        const bb = busboy({ headers: req.headers });
+    
+        let clippings: Clipping[];
+    
+        bb.on("file", (name, file) => {
+            console.log(`[${name}] found, starting import.`);
+            importClippingsUsecase(new File(file)).then(c => clippings = c);
+        });
+    
+        bb.on("close", () => {
+            res.status(201).json({ data: clippings.map((c) => c.toDTO()) });
+        });
+    
+        req.pipe(bb);
+    }
+} 
+
